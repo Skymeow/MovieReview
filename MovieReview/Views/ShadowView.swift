6bba6f2aa5ae61bab55ece77ecf223da9369c796
Sky Xu <skyrocks88xmz@gmail.com>
2019-05-19 15:38:58 -0700

    fix shadow offset

--- MovieReview/Views/ShadowView.swift
+++ MovieReview/Views/ShadowView.swift
@@ -23,9 +23,9 @@ class ShadowView: UIView {
     
     private func setupShadow() {
         self.layer.cornerRadius = Constants.radius
-        self.layer.shadowOffset = CGSize(width: 0, height: 3)
+        self.layer.shadowOffset = CGSize(width: 1, height: 2)
         self.layer.shadowRadius = 3
-        self.layer.shadowOpacity = 0.25
+        self.layer.shadowOpacity = 0.35
         self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: .allCorners, cornerRadii: CGSize(width: Constants.radius, height: Constants.radius)).cgPath
         self.layer.shouldRasterize = true
         self.layer.rasterizationScale = UIScreen.main.scale
